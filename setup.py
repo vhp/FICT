97d794561bdaa322cca60a2925cb360c18e897bf
Vincent Perricone <vhp@fastmail.fm>
2023-06-06 22:05:07 -0400

    update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name = 'Fict',
-    version = "1.0.0",
+    version = "1.0.1",
     description = 'File integrity checking tool',
     long_description=long_description,
     long_description_content_type='text/markdown',
